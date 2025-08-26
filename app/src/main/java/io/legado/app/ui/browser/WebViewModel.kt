dd3e120f435a3cceeb9ed0393f3c6b5f0e2335d6
wangqz <2809755637@qq.com>
2025-08-26 18:10:08 +0800

    init: 阅读 legado 内部集成 edge tts

---
+++ app/src/main/java/io/legado/app/ui/browser/WebViewModel.kt
@@ -0,0 +1,148 @@
+package io.legado.app.ui.browser
+
+import android.app.Application
+import android.content.Intent
+import android.net.Uri
+import android.util.Base64
+import android.webkit.URLUtil
+import android.webkit.WebView
+import androidx.documentfile.provider.DocumentFile
+import io.legado.app.base.BaseViewModel
+import io.legado.app.constant.AppConst
+import io.legado.app.constant.SourceType
+import io.legado.app.data.appDb
+import io.legado.app.exception.NoStackTraceException
+import io.legado.app.help.http.newCallResponseBody
+import io.legado.app.help.http.okHttpClient
+import io.legado.app.help.source.SourceHelp
+import io.legado.app.help.source.SourceVerificationHelp
+import io.legado.app.model.analyzeRule.AnalyzeUrl
+import io.legado.app.utils.DocumentUtils
+import io.legado.app.utils.FileUtils
+import io.legado.app.utils.isContentScheme
+import io.legado.app.utils.printOnDebug
+import io.legado.app.utils.toastOnUi
+import io.legado.app.utils.writeBytes
+import org.apache.commons.text.StringEscapeUtils
+import java.io.File
+import java.util.Date
+
+class WebViewModel(application: Application) : BaseViewModel(application) {
+    var intent: Intent? = null
+    var baseUrl: String = ""
+    var html: String? = null
+    val headerMap: HashMap<String, String> = hashMapOf()
+    var sourceVerificationEnable: Boolean = false
+    var refetchAfterSuccess: Boolean = true
+    var sourceName: String = ""
+    var sourceOrigin: String = ""
+    var sourceType = SourceType.book
+
+    fun initData(
+        intent: Intent,
+        success: () -> Unit
+    ) {
+        execute {
+            this@WebViewModel.intent = intent
+            val url = intent.getStringExtra("url")
+                ?: throw NoStackTraceException("url不能为空")
+            sourceName = intent.getStringExtra("sourceName") ?: ""
+            sourceOrigin = intent.getStringExtra("sourceOrigin") ?: ""
+            sourceType = intent.getIntExtra("sourceType", SourceType.book)
+            sourceVerificationEnable = intent.getBooleanExtra("sourceVerificationEnable", false)
+            refetchAfterSuccess = intent.getBooleanExtra("refetchAfterSuccess", true)
+            val source = SourceHelp.getSource(sourceOrigin, sourceType)
+            val analyzeUrl = AnalyzeUrl(url, source = source, coroutineContext = coroutineContext)
+            baseUrl = analyzeUrl.url
+            headerMap.putAll(analyzeUrl.headerMap)
+            if (analyzeUrl.isPost()) {
+                html = analyzeUrl.getStrResponseAwait(useWebView = false).body
+            }
+        }.onSuccess {
+            success.invoke()
+        }.onError {
+            context.toastOnUi("error\n${it.localizedMessage}")
+            it.printOnDebug()
+        }
+    }
+
+    fun saveImage(webPic: String?, path: String) {
+        webPic ?: return
+        execute {
+            val fileName = "${AppConst.fileNameFormat.format(Date(System.currentTimeMillis()))}.jpg"
+            webData2bitmap(webPic)?.let { biteArray ->
+                if (path.isContentScheme()) {
+                    val uri = Uri.parse(path)
+                    DocumentFile.fromTreeUri(context, uri)?.let { doc ->
+                        DocumentUtils.createFileIfNotExist(doc, fileName)
+                            ?.writeBytes(context, biteArray)
+                    }
+                } else {
+                    val file = FileUtils.createFileIfNotExist(File(path), fileName)
+                    file.writeBytes(biteArray)
+                }
+            } ?: throw Throwable("NULL")
+        }.onError {
+            context.toastOnUi("保存图片失败:${it.localizedMessage}")
+        }.onSuccess {
+            context.toastOnUi("保存成功")
+        }
+    }
+
+    private suspend fun webData2bitmap(data: String): ByteArray? {
+        return if (URLUtil.isValidUrl(data)) {
+            okHttpClient.newCallResponseBody {
+                url(data)
+            }.bytes()
+        } else {
+            Base64.decode(data.split(",").toTypedArray()[1], Base64.DEFAULT)
+        }
+    }
+
+    fun saveVerificationResult(webView: WebView, success: () -> Unit) {
+        if (!sourceVerificationEnable) {
+            return success.invoke()
+        }
+        if (refetchAfterSuccess) {
+            execute {
+                val url = intent!!.getStringExtra("url")!!
+                val source = appDb.bookSourceDao.getBookSource(sourceOrigin)
+                html = AnalyzeUrl(
+                    url,
+                    headerMapF = headerMap,
+                    source = source,
+                    coroutineContext = coroutineContext
+                ).getStrResponseAwait(useWebView = false).body
+                SourceVerificationHelp.setResult(sourceOrigin, html ?: "")
+            }.onSuccess {
+                success.invoke()
+            }
+        } else {
+            webView.evaluateJavascript("document.documentElement.outerHTML") {
+                execute {
+                    html = StringEscapeUtils.unescapeJson(it).trim('"')
+                    SourceVerificationHelp.setResult(sourceOrigin, html ?: "")
+                }.onSuccess {
+                    success.invoke()
+                }
+            }
+        }
+    }
+
+    fun disableSource(block: () -> Unit) {
+        execute {
+            SourceHelp.enableSource(sourceOrigin, sourceType, false)
+        }.onSuccess {
+            block.invoke()
+        }
+    }
+
+    fun deleteSource(block: () -> Unit) {
+        execute {
+            SourceHelp.deleteSource(sourceOrigin, sourceType)
+        }.onSuccess {
+            block.invoke()
+        }
+    }
+
+}
